52a6b7b8598cf3bdab4970af31a04198c659f9f6
Alejandra Ro <ale.ro.shegiwan@gmail.com>
2022-08-13 09:35:54 -0500

    Se agregaron carpetas y ejercicios

---
+++ node/1.5Modules-impor-export/es6-import-export/modules/sum-substract.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+module.exports = {//esto es un objeto cuando se quieren exportar muchas cosas
+    sum:sum,
+    substract,
+};
